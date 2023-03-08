a265963ed4f80d2f1c37feb132ecbefa97ff5fb4
Oshomah Abubakar <51214370+Oshomah26@users.noreply.github.com>
2023-03-08 21:50:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ function getJokes(e){
 
             if (response.error === false) {
                 response.jokes.forEach(function(joke){
-                    output += `<li>${joke.setup}</li>
-                               <li>${joke.delivery}</li>`;
+                    output += joke.type === 'single' 
+                    ? `<li>${joke.joke}</li>` 
+                    : `<li>${joke.setup}</li>
+                      <small style = "text-indent:10px"><li style = "list-style-type: square">${joke.delivery}</li></small>`;
                 });
             }else{
                 output += '<li>Something went wrong</li>'
